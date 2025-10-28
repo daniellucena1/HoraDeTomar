dfaf880501908dfafd2a770003b43850d8d0db30
daniellucena1 <daniellucena.jt@gmail.com>
2025-10-28 15:22:15 -0300

    fix: consertar erro de scroll do header

--- app/src/main/java/br/upe/horaDeTomar/ui/MainScreen.kt
+++ app/src/main/java/br/upe/horaDeTomar/ui/MainScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -70,6 +71,11 @@ fun MainScreen(viewModel: AccountViewModel = hiltViewModel()) {
                 TopAppBarDefaults.exitUntilCollapsedScrollBehavior(topBarState)
             } else {
                 TopAppBarDefaults.pinnedScrollBehavior(topBarState)
+            }
+
+            LaunchedEffect(currentRoute) {
+                topBarState.heightOffset = 0f
+                topBarState.contentOffset = 0f
             }
 
             Scaffold(
